0f7467c31edd39b467650557eafd325465ff1881
shoniisra-pichincha <jivillac@pichincha.com>
2021-07-16 14:32:24 -0500

    agrego plantilla

---
+++ components/slices/index.js
@@ -0,0 +1,15 @@
+import Hero from "./hero";
+import HeroGradient from "./heroGradient";
+import CallToAction from "./callToAction";
+import Cards from "./cards";
+import ImageAndText from "./imageAndText";
+import Pricing from "./pricing";
+
+export {
+  Hero,  
+  HeroGradient,
+  CallToAction,
+  Cards,
+  ImageAndText,
+  Pricing,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Page from "@components/common/page";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="w-full">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>
+          Tailwind Starter Template - Landing Page Template: Tailwind Toolbox
+        </title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="" />
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <Header/>
+      <Page></Page>      
+      <Footer /> 
     </div>
-  )
+  );
 }
