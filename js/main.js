44eff869c4285d019310a92ddfee4c1d2ee0b7a6
shoniisra <jivillac@pichincha.com>
2022-08-19 21:31:33 -0500

    agrego opcion de traducir

--- js/main.js
+++ js/main.js
@@ -121,6 +121,11 @@
         loop: true,
     });
 
-    
+    //tooltip
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
 })(jQuery);
 
